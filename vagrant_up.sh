af3a7791b5a0dc6d92e59466cc1db9977a6698f6
Bernd Lehmann <bernd@lehmann.be>
2017-12-20 18:49:40 +0100

    add vagrant.sh

---
+++ vagrant_up.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+EXIT=0
+vagrant up rtr-1 --color <<< 'rtr-1 boot' || EXIT=$?
+vagrant up rtr-2 --color <<< 'rtr-2 boot' || EXIT=$?
+vagrant up rtr-3 --color <<< 'asa-1 boot' || EXIT=$?
+vagrant up rtr-4 --color <<< 'asa-1 boot' || EXIT=$?
+echo $EXIT
+exit $EXIT
