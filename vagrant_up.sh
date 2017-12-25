310f51c2d9e840dbabaa02ebf2ee3c36ae3d6ae4
Bernd Lehmann <bernd@lehmann.be>
2017-12-25 14:18:20 +0100

    changed vagrant_up.sh

--- vagrant_up.sh
+++ vagrant_up.sh
@@ -3,7 +3,7 @@
 EXIT=0
 vagrant up rtr-1 --color <<< 'rtr-1 boot' || EXIT=$?
 vagrant up rtr-2 --color <<< 'rtr-2 boot' || EXIT=$?
-vagrant up rtr-3 --color <<< 'asa-1 boot' || EXIT=$?
-vagrant up rtr-4 --color <<< 'asa-1 boot' || EXIT=$?
+vagrant up rtr-3 --color <<< 'rtr-3 boot' || EXIT=$?
+vagrant up rtr-4 --color <<< 'rtr-4 boot' || EXIT=$?
 echo $EXIT
 exit $EXIT
